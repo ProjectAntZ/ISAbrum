154a70a7f763660b090c9676c159e84d20fb87e9
ProjectAntZ <76040178+ProjectAntZ@users.noreply.github.com>
2021-12-30 14:01:21 +0100

    New model

--- model_creator_CNN.py
+++ model_creator_CNN.py
@@ -21,19 +21,18 @@ def create_model():
     x = tf.keras.layers.Rescaling(1. / 255)(x)
     # x = tf.keras.layers.LayerNormalization(axis=3)(x)
 
-    size = [4, 4]
+    size = [3, 3]
     for i, s in enumerate(size):
         for _ in range(1):
             x = tf.keras.layers.DepthwiseConv2D(depth_multiplier=s, kernel_size=(5, 5), padding='same', activation='relu')(x)
             x = tf.keras.layers.BatchNormalization()(x)
-            x = tf.keras.layers.GaussianDropout(1. / 4)(x)
+            # x = tf.keras.layers.GaussianDropout(1. / 8)(x)
         if i != len(size)-1:
-            x = tf.keras.layers.SpatialDropout2D(1. / 4)(x)
+            # x = tf.keras.layers.SpatialDropout2D(1. / 8)(x)
             x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
 
-    x = tf.keras.layers.Conv2D(filters=96, kernel_size=(5, 5), padding='same', activation='relu')(x)
-    x = tf.keras.layers.Conv2D(filters=96, kernel_size=(5, 5), padding='same', activation='relu')(x)
-    x = tf.keras.layers.GaussianDropout(1. / 4)(x)
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(5, 5), padding='same', activation='relu')(x)
+    # x = tf.keras.layers.GaussianDropout(1. / 8)(x)
 
     '''x = input = tf.keras.layers.Input(shape=frameResolution + (3,))
     x = tf.keras.layers.Rescaling(1. / 255)(x)
@@ -49,12 +48,12 @@ def create_model():
                 x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)'''
 
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dropout(1. / 3)(x)
+    #x = tf.keras.layers.Dropout(1. / 4)(x)
     x = tf.keras.layers.Dense(
-        units=96,
+        units=32,
         activation='relu',
     )(x)
-    # x = tf.keras.layers.Dropout(1. / 2)(x)
+    x = tf.keras.layers.Dropout(1. / 2)(x)
     x = tf.keras.layers.Dense(
         units=1,
         activation='sigmoid',
